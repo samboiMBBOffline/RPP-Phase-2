5d4d60f878876d3e68545542069c2c1059b4f156
Mohd Aisamudin Bin Mohd Akfan <mohd.aisamudin@maybank.com>
2022-12-16 09:19:06 +0800

    16/12

---
+++ Object Repository/DuitNowRequest/InputNo.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>InputNo</name>
+   <tag></tag>
+   <elementGuidId>448649c3-71c8-4b66-b521-0506d898cb95</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>android.widget.EditText</value>
+      <webElementGuid>5160dacc-90cf-4108-b4ad-00633e373774</webElementGuid>
+   </webElementProperties>
+   <locator>(//*[@class = 'android.widget.EditText'])[${No}]</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
--- Scripts/1a. DuitNowRequest - SoleProp Registered/Script1671002825470.groovy
+++ Scripts/1a. DuitNowRequest - SoleProp Registered/Script1671002825470.groovy
@@ -20,6 +20,8 @@ import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import com.my.android.keyword.tapNo as tapNo
 import com.my.android.keyword.password as password
 
+VerifyFull = ft('UserID').getValue(6,1)
+
 'Tap DuitNowRequest'
 Mobile.tap(fo('Text', [('Verify') : ft('TestData').getValue(4, 5)]), 0)
 
@@ -32,6 +34,8 @@ Mobile.takeScreenshot('Screenshot/DRR/2.png')
 
 'ID type'
 Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 8)]), 0),ft('TestData').getValue(4, 8) )
+
+if(VerifyFull == 'Y') {
 
 Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
 
@@ -85,10 +89,123 @@ Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('Dropdown').getVal
 
 Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
 
+Mobile.tap(fo('Text',[('Verify'):ft('Dropdown').getValue(2,2)]),0)
+
 'tap mobile no'
 Mobile.tap(fo('Text',[('Verify'):'Done']),0)
 
+'Mobile Number'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 10)]), 0),ft('TestData').getValue(4, 10))
 
+'Enter Number'
+Mobile.verifyEqual(Mobile.getText(fo('Input', [('Verify') : ft('TestData').getValue(5, 10)]), 0),ft('TestData').getValue(5, 10))
+
+Mobile.tap(fo('Object Repository/DuitNowRequest/ContactNo Button'),0)
+
+Mobile.takeScreenshot('Screenshot/DDR/6.png')
+
+Mobile.pressBack()
+
+Mobile.setText(fo('InputNo',[('No'):1]),ft('TestData').getValue(6,10),0)
+
+'check format'
+Mobile.verifyEqual(Mobile.getText(fo('InputNo',[('No'):1]),0),ft('TestData').getValue(7,10))
+
+Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
+
+Mobile.tap(fo('Text',[('Verify'):ft('Dropdown').getValue(2,3)]),0)
+
+'tap mobile no'
+Mobile.tap(fo('Text',[('Verify'):'Done']),0)
+
+'NRIC Number'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 11)]), 0),ft('TestData').getValue(4, 11))
+
+'Enter NRIC'
+Mobile.verifyEqual(Mobile.getText(fo('Input', [('Verify') : ft('TestData').getValue(5, 11)]), 0),ft('TestData').getValue(5, 11))
+
+Mobile.setText(fo('InputNo',[('No'):1]),ft('TestData').getValue(6,11),0)
+
+'Check format'
+Mobile.verifyEqual(Mobile.getText(fo('InputNo',[('No'):1]),0),ft('TestData').getValue(7,11))
+
+
+
+Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
+
+Mobile.tap(fo('Text',[('Verify'):ft('Dropdown').getValue(2,4)]),0)
+
+'tap pass no'
+Mobile.tap(fo('Text',[('Verify'):'Done']),0)
+
+Mobile.hideKeyboard()
+
+'NRIC Number'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 12)]), 0),ft('TestData').getValue(4, 12))
+
+'Enter NRIC'
+Mobile.verifyEqual(Mobile.getText(fo('Input', [('Verify') : ft('TestData').getValue(5, 12)]), 0),ft('TestData').getValue(5, 12))
+
+'NRIC Number'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 13)]), 0),ft('TestData').getValue(4, 13))
+
+'Enter NRIC'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(5, 13)]), 0),ft('TestData').getValue(5, 13))
+
+'tap country'
+Mobile.tap(fo('Text', [('Verify') : ft('TestData').getValue(5, 13)]), 0)
+
+for(i=1;i<4;i++) {
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('Dropdown').getValue(3, i)]), 0),ft('Dropdown').getValue(3, i))
+}
+Mobile.tap(fo('Text',[('Verify'):'Done']),0)
+
+Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
+
+Mobile.tap(fo('Text',[('Verify'):ft('Dropdown').getValue(2,5)]),0)
+
+'tap pass no'
+Mobile.tap(fo('Text',[('Verify'):'Done']),0)
+
+'Army Police ID'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 14)]), 0),ft('TestData').getValue(4, 14))
+
+'Enter Army Police ID'
+Mobile.verifyEqual(Mobile.getText(fo('Input', [('Verify') : ft('TestData').getValue(5, 14)]), 0),ft('TestData').getValue(5, 14))
+
+Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
+
+Mobile.tap(fo('Text',[('Verify'):ft('Dropdown').getValue(2,6)]),0)
+
+'tap pass no'
+Mobile.tap(fo('Text',[('Verify'):'Done']),0)
+
+'Army Police ID'
+Mobile.verifyEqual(Mobile.getText(fo('Text', [('Verify') : ft('TestData').getValue(4, 15)]), 0),ft('TestData').getValue(4, 15))
+
+'Enter Army Police ID'
+Mobile.verifyEqual(Mobile.getText(fo('Input', [('Verify') : ft('TestData').getValue(5, 15)]), 0),ft('TestData').getValue(5, 15))
+
+
+}
+Mobile.tap(fo('Object Repository/DuitNowRequest/IDtype'),0)
+
+Mobile.scrollToText(ft('TestData').getValue(7,1),FailureHandling.CONTINUE_ON_FAILURE)
+
+Mobile.tap(fo('Text',[('Verify'):ft('TestData').getValue(7,1)]),0)
+
+'tap pass no'
+Mobile.tap(fo('Text',[('Verify'):'Done']),0)
+
+if(Mobile.getText(fo('Object Repository/DuitNowRequest/IDtype'),0)=='Mobile Number'||
+	Mobile.getText(fo('Object Repository/DuitNowRequest/IDtype'),0)=='NRIC Number'||
+	Mobile.getText(fo('Object Repository/DuitNowRequest/IDtype'),0)=='Army/Police ID'||
+	Mobile.getText(fo('Object Repository/DuitNowRequest/IDtype'),0)=='Business Registration Number') {
+	
+	Mobile.setText(fo('InputNo',[('No'):1]),ft('TestData').getValue(7,2),0)
+	
+	
+}
 
 
 
